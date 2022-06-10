1149315b25f69d729b75bab407841d327b9cfe8e
Abek_Dev <abek01sulaymonov@gmail.com>
2022-06-10 17:25:38 +0500

    Added mongodb to project

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 import express from "express";
 import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
 
